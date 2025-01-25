644a683a6f82b49fc65224dc5278176cf5941dc0
Andy Le <4ndyle@gmail.com>
2025-01-25 00:49:46 -0700

    update picture and add blur effect to text

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,27 +1,35 @@
+import BlurFade from "./ui/blur-fade";
+
 function Info() {
   return (
     <section className="flex items-center max-md:flex-col">
       <div className="border-black w-[50%] pr-24 max-lg:pr-16 max-lg:w-[60%] max-md:px-4 max-md:w-full">
-        <p className="aboutDesc">
-          I am currently a undergraduate student with a passion for
-          <strong> design and software development</strong>.
-        </p>
+        <BlurFade>
+          <p className="aboutDesc">
+            I am currently a undergraduate student with a passion for
+            <strong> design and software development</strong>.
+          </p>
+        </BlurFade>
         <br></br>
-        <p className="aboutDesc">
-          With both of these passions, I gained experience being a{" "}
-          <strong>UI/UX designer and web developer</strong>! I enjoy the process
-          of mapping all of my designs and ideas onto various design interfaces
-          and translating these designs into a website through code. The process
-          of troubleshooting, adjusting, and implementing designs into scalable
-          and usable applications is astounding to me.
-        </p>
+        <BlurFade delay={0.5}>
+          <p className="aboutDesc">
+            With both of these passions, I gained experience being a{" "}
+            <strong>UI/UX designer and web developer</strong>! I enjoy the
+            process of mapping all of my designs and ideas onto various design
+            interfaces and translating these designs into a website through
+            code. The process of troubleshooting, adjusting, and implementing
+            designs into scalable and usable applications is astounding to me.
+          </p>
+        </BlurFade>
         <br></br>
-        <p className="aboutDesc">
-          The biggest reason why I am pursing a career in tech is because of the
-          evolving architecture. I love being able to constantly improve and
-          evolve day by day. There’s always so much do learn and do, with
-          endless opportunities to grow!
-        </p>
+        <BlurFade delay={1}>
+          <p className="aboutDesc">
+            The biggest reason why I am pursing a career in tech is because of
+            the evolving architecture. I love being able to constantly improve
+            and evolve day by day. There’s always so much do learn and do, with
+            endless opportunities to grow!
+          </p>
+        </BlurFade>
       </div>
       <Images />
     </section>
@@ -37,7 +45,7 @@ function Images() {
       </div>
       <div className="flex flex-col gap-8 w-2/5 max-lg:hidden">
         <img src="profile2.jpg" className="rounded-3xl max-lg:hidden" />
-        <img src="profile3.jpeg" className="rounded-3xl max-lg:hidden" />
+        <img src="boston.jpg" className="rounded-3xl max-lg:hidden" />
       </div>
     </div>
   );
