746d9fae17fefe18a57be81a62cc6932fdda05d7
Andy Le <4ndyle@gmail.com>
2025-01-25 22:42:31 -0700

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
 import "./App.css";
-
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@radix-ui/themes/styles.css";
-import Lander from "./components/Lander";
-import Nav from "./components/Nav";
-import Projects from "./components/Projects";
-import Experiences from "./components/Experiences";
-import Footer from "./components/Footer";
+import ScrollToTop from "./ScrollToTop";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
-  const [bgColor, setBgColor] = useState("black");
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPoint = 750;
-      if (window.scrollY > scrollPoint) {
-        setBgColor("#EEF1F4"); // New background color
-      } else {
-        setBgColor("black"); // Original background color
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <section
-      style={{
-        backgroundColor: bgColor,
-        transition: "background-color 1s ease",
-      }}
-    >
-      <Nav />
-      <Lander />
-
-      <section className="flex flex-col mx-[15vw] max-sm:mx-[10vw] justify-center items-center">
-        <Projects />
-        <Experiences />
-        <Footer />
-      </section>
-    </section>
+    <Router>
+      {/* <ScrollToTop /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./About.tsx";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import About from "./About.tsx";
+// import Home from "./Home.tsx";
 import "@radix-ui/themes/styles.css";
+import App from "./App.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "about",
+//     element: <About />,
+//   },
+// ]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App />
   </StrictMode>
 );
