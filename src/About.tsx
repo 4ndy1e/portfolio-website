a60712e3ec477ca4c386f230dcd193199f280f1e
Andy Le <4ndyle@gmail.com>
2025-01-25 23:05:11 -0700

    add typewriter effect to about me page

--- src/About.tsx
+++ src/About.tsx
@@ -3,16 +3,43 @@ import Info from "./components/Info.jsx";
 import Education from "./components/Education";
 import Hobbies from "./components/Hobbies";
 import Footer from "./components/Footer.jsx";
+import { TypewriterEffect } from "./components/ui/type-writer.js";
 
 function AboutPage() {
+  const words = [
+    {
+      text: "Hi,",
+      className:
+        "text-[75px] text-black text-left max-md:text-center text-md:text-[60px] max-[570px]:text-[50px]",
+    },
+    {
+      text: "I'm",
+      className:
+        "text-[75px] text-black text-left max-md:text-center text-md:text-[60px] max-[570px]:text-[50px]",
+    },
+    {
+      text: "Andy...",
+      className:
+        "text-[75px] text-black text-left max-md:text-center text-md:text-[60px] max-[570px]:text-[50px]",
+    },
+  ];
   return (
     <section className="flex flex-col mx-[5vw] max-sm:mx-[10vw] items-center mt-48 max-[570px]:mx-6 ">
       <Nav />
-      <div className="w-full mb-48 max-[570px]:mb-24">
+      {/* <div className="w-full mb-48 max-[570px]:mb-24">
         <h1 className="text-black text-left max-md:text-center text-md:text-[60px] max-[570px]:text-[50px]">
           {" "}
           Hi, I'm Andy...
         </h1>
+      </div> */}
+      <div className="flex w-full mb-48 max-[570px]:mb-24">
+        <div className="text-black text-left max-md:text-center text-md:text-[60px] max-[570px]:text-[50px]">
+          <TypewriterEffect
+            words={words}
+            cursorClassName="text-[75px]"
+            className="text-[75px]"
+          />
+        </div>
       </div>
       <div className="w-full m-24">
         <p className="text-left border-t-2 text-[#b4b4b4] font-bold text-2xl">
