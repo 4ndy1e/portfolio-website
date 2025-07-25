0efb7b9e36c80395e878b436b561e776eaa63db1
Andy Le <4ndyle@gmail.com>
2025-07-25 00:38:47 -0700

    Remove effects on lander

--- src/components/Lander.tsx
+++ src/components/Lander.tsx
@@ -4,29 +4,6 @@ import Particles from "@/blocks/Backgrounds/Particles/Particles";
 function Lander() {
   return (
     <section className="relative flex flex-col items-center justify-center h-[100vh] mb-[400px]">
-      {/* Aurora as background */}
-      <div className="absolute inset-0 z-0 h-[50%]">
-        <Aurora
-          colorStops={["#5227FF", "#7CFF67", "#5227FF"]}
-          blend={0.5}
-          amplitude={0.5}
-          speed={1}
-        />
-      </div>
-
-      <div className="absolute inset-0 z-0">
-        <Particles
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={400}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={75}
-          moveParticlesOnHover={false}
-          alphaParticles={false}
-          disableRotation={true}
-        />
-      </div>
-
       {/* Content layer */}
       <div className="relative z-10 max-w-[900px] flex-col text-left items-center justify-center">
         <h2 className="text-white text-3xl font-bold font-['Inter'] max-lg:text-2xl max-sm:text-xl">
