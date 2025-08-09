48c23bc0313d50b927a4c592f34b81c747cdb290
Andy Le <4ndyle@gmail.com>
2025-08-08 23:21:19 -0700

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
     <section className="mt-96 pb-48 w-full">
       <div>
         <h2 className="text-7xl font-bold my-16 text-left mb-4 max-[500px]:text-5xl max-[550px]:text-center">
-          Thank You.
+          The End.
         </h2>
         <p className="text-left text-[#B4B4B4] text-xl max-[550px]:text-center max-[500px]:text-lg">
           {" "}
